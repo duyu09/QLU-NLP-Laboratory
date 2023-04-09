fbee6a4dea64a4a7c680d2af4dfd3d8702d8376d
Duyu <92843163+duyu09@users.noreply.github.com>
2023-04-09 12:15:33 +0800

    Add files via upload

---
+++ sql/nlp_lab_brief.sql
@@ -0,0 +1,36 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50736
+ Source Host           : localhost:3306
+ Source Schema         : nlp
+
+ Target Server Type    : MySQL
+ Target Server Version : 50736
+ File Encoding         : 65001
+
+ Date: 07/04/2023 02:54:33
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for nlp_lab_brief
+-- ----------------------------
+DROP TABLE IF EXISTS `nlp_lab_brief`;
+CREATE TABLE `nlp_lab_brief`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `record_content` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '实验室简介',
+  `post_sort` int(255) NULL DEFAULT NULL COMMENT '显示顺序',
+  `status` char(6) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '状态（0正常 1停用）',
+  `creat_by` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建者',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '更新者',
+  `updata_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
